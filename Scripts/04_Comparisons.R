f2dc5078798d79d172b243047f4e4a7700e7d26e
Ryan <ryan.a.opel@gmail.com>
2017-08-02 13:40:28 -0700

    Initial commit

---
+++ Scripts/04_Comparisons.R
@@ -0,0 +1,10 @@
+## Script: Comparison of Custom Analysis Factors to Actiware's
+## Project: actigraphy -- H4085
+## Author: Ryan Opel -- @opelr
+## Date: 2017-08-02
+## Version: 0.1.0
+
+library(magrittr)
+library(tidyverse)
+library(lubridate)
+library(reshape2)
